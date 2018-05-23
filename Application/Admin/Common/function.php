6a6bbb8449626ff173d5e4d0b1b7ffc0276f699e
USER-20170315EF\Administrator <duanyng@gmail.com>
2018-05-23 12:50:18 +0800

    active menu

--- Application/Admin/Common/function.php
+++ Application/Admin/Common/function.php
@@ -7,7 +7,7 @@
 function get_nav()
 {
     $navs = C('navs.relation');
-    $li = '';
+    $li   = '';
     foreach ($navs as $val) {
         $active = '';
         if (in_array(CONTROLLER_NAME, $val['lists'])) {
@@ -17,7 +17,6 @@ function get_nav()
     }
     return $li;
 }
-
 
 function admin_left_nav()
 {
@@ -31,21 +30,20 @@ function admin_left_nav()
                     if (isset($item['icon'])) {
                         $icon = '<i class="fa ' . $item['icon'] . '"></i>&nbsp;';
                     }
-                    $class = '';
+                    $class     = '';
                     $nav_items = get_admin_left_nav_items($item['uri']);
                     if ($nav_items['class']) {
                         $class = ' class="' . $nav_items['class'] . '"';
                     }
                     $nav .= '<li' . $class . '><a href="' . $nav_items['url'] . '">' . $icon . $item['name'] . '</a></li>';
                 }
-            }
-            else {
+            } else {
                 $nav = '';
                 foreach ($navs[$key]['items'] as $items) {
-                    $active = '';
+                    $active   = '';
                     $collapse = ' collapse ';
                     if (in_array(CONTROLLER_NAME, explode(',', $items['lists']))) {
-                        $active = ' class="active"';
+                        $active   = ' class="active"';
                         $collapse = '';
                     }
                     $level_top_icon = '';
@@ -54,12 +52,23 @@ function admin_left_nav()
                     }
                     $ul = '<ul id="secondNav' . $items['group'] . '" class="nav nav-list ' . $collapse . ' secondmenu">';
                     foreach ($items['items'] as $item) {
-                        $nav_items = get_admin_left_nav_items($item['uri']);
+                        // active second item
+                        $secondActive = '';
+                        if ($active) {
+                            $uri       = $item['uri'];
+                            $uriArr    = explode('.', $uri);
+                            $uriString = isset($uriArr[0]) ? $uriArr[0] : null;
+                            $nowUri    = CONTROLLER_NAME . '/' . ACTION_NAME;
+                            if ($uriString == $nowUri) {
+                                $secondActive = 'active';
+                            }
+                        }
+                        $nav_items         = get_admin_left_nav_items($item['uri']);
                         $level_second_icon = '';
                         if (isset($item['icon'])) {
                             $level_second_icon = '<i class="fa ' . $item['icon'] . '"></i>&nbsp;';
                         }
-                        $ul .= '<li class="' . $nav_items['class'] . '"><a href="' . $nav_items['url'] . '">' .
+                        $ul .= '<li class="' . $nav_items['class'] . ' ' . $secondActive . '"><a href="' . $nav_items['url'] . '">' .
                             $level_second_icon . $item['name'] . '</a></li>';
                     }
                     $ul .= '</ul>';
@@ -75,26 +84,27 @@ function admin_left_nav()
 
 function get_admin_left_nav_items($uri)
 {
-    $uris = explode('/', $uri);
+    $uris  = explode('/', $uri);
     $class = '';
-    if (strpos($uris[1], '.')) {// 本身uri中自带的class
+    if (strpos($uris[1], '.')) {
+// 本身uri中自带的class
         $classes = explode('.', $uris[1]);
-        $url = U($uris[0] . '/' . $classes[0]);
+        $url     = U($uris[0] . '/' . $classes[0]);
         if (CONTROLLER_NAME == $uris[0] && ACTION_NAME == $classes[0]) {
-            $class = 'active';// 访问当前url，去掉自带的class，并设置为active
+            $class = 'active'; // 访问当前url，去掉自带的class，并设置为active
         } else {
             unset($classes[0]);
             $class .= join($classes, ' ');
         }
     } else {
         if (CONTROLLER_NAME == $uris[0] && ACTION_NAME == $uris[1]) {
-            $class = 'active';// 访问当前url，去掉自带的class，并设置为active
+            $class = 'active'; // 访问当前url，去掉自带的class，并设置为active
         }
         $url = U($uri);
     }
     return [
         'class' => $class,
-        'url' => $url,
+        'url'   => $url,
     ];
 }
 
@@ -102,13 +112,3 @@ function check_admin_login()
 {
     return D('Manage')->checkAdminLogin();
 }
-
-
-
-
-
-
-
-
-
-
--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -1,22 +1,19 @@
 <?php
 
-
-
-
 return array(
     //'配置项'=>'配置值'
-//    'LAYOUT_ON' => true,
-//    'LAYOUT_NAME' => 'layout',
-//    'LAYOUT_NAME'=>'Layout/layout',
+    //    'LAYOUT_ON' => true,
+    //    'LAYOUT_NAME' => 'layout',
+    //    'LAYOUT_NAME'=>'Layout/layout',
 
     'navs' => [
         'relation' => [
-            'system' => ['name' => '系统', 'link' => 'Index/index', 'lists' => ['Index']],
-            'users' => ['name' => '用户', 'link' => 'Manage/index', 'lists' => ['Manage', 'Group', 'Logs', 'User']],
-            'content' => ['name' => '内容', 'link' => 'Article/index', 'lists'  => ['Article', 'Comment', 'Category']],
-            'model' => ['name' => '应用', 'link' => 'DB/index', 'lists'  => ['DB', 'Addons']],
+            'system'  => ['name' => '系统', 'link' => 'Index/index', 'lists' => ['Index']],
+            'users'   => ['name' => '用户', 'link' => 'Manage/index', 'lists' => ['Permission', 'Manage', 'Group', 'Logs', 'User']],
+            'content' => ['name' => '内容', 'link' => 'Article/index', 'lists' => ['Article', 'Comment', 'Category']],
+            'model'   => ['name' => '应用', 'link' => 'DB/index', 'lists' => ['DB', 'Addons']],
         ],
-        'system' => [//系统
+        'system'   => [ //系统
             'level' => 1,
             'items' => [
                 ['name' => '首页', 'uri' => 'Index/index', 'icon' => 'fa-home'],
@@ -24,77 +21,91 @@ return array(
                 ['name' => '专用于测试', 'uri' => 'Index/test.hide', 'icon' => 'fa-cogs'],
             ],
         ],
-        'users' => [//用户
+        'users'    => [ //用户
             'level' => 2,
             'items' => [
                 [
-                    'name' => '后台用户', 'group' => 'Manage', 'icon' => 'fa-user-md', 'lists' => 'Manage,Logs',
+                    'name'  => '权限管理', 'group' => 'Permission', 'icon' => 'fa-users', 'lists' => 'Permission,Role',
+                    'items' => [
+                        ['name' => '权限列表', 'uri' => 'Permission/index', 'icon' => 'fa-list-alt'],
+                        ['name' => '添加权限', 'uri' => 'Permission/add', 'icon' => 'fa-plus-circle'],
+                        ['name' => '修改权限', 'uri' => 'Permission/edit.hide', 'icon' => 'fa-pencil-square'],
+                        ['name' => '权限回收站', 'uri' => 'Permission/recycle', 'icon' => 'fa-recycle'],
+
+                        ['name' => '角色列表', 'uri' => 'Role/index', 'icon' => 'fa-list-alt'],
+                        ['name' => '添加角色', 'uri' => 'Role/add', 'icon' => 'fa-plus-circle'],
+                        ['name' => '修改角色', 'uri' => 'Role/edit.hide', 'icon' => 'fa-pencil-square'],
+                        ['name' => '角色回收站', 'uri' => 'Role/recycle', 'icon' => 'fa-recycle'],
+                    ],
+                ],
+                [
+                    'name'  => '后台用户', 'group' => 'Manage', 'icon' => 'fa-user-md', 'lists' => 'Manage,Logs',
                     'items' => [
                         ['name' => '管理员列表', 'uri' => 'Manage/index', 'icon' => 'fa-list-alt'],
                         ['name' => '添加管理员', 'uri' => 'Manage/add', 'icon' => 'fa-plus-circle'],
                         ['name' => '修改管理员资料', 'uri' => 'Manage/edit.hide', 'icon' => 'fa-pencil-square'],
                         ['name' => '管理员回收站', 'uri' => 'Manage/recycle', 'icon' => 'fa-recycle'],
                         ['name' => '后台用户行为', 'uri' => 'Logs/index', 'icon' => 'fa-pencil-square-o'],
-                    ]
+                    ],
                 ],
                 [
-                    'name' => '注册用户', 'group' => 'User', 'icon' => 'fa-users', 'lists' => 'User',
+                    'name'  => '注册用户', 'group' => 'User', 'icon' => 'fa-users', 'lists' => 'User',
                     'items' => [
                         ['name' => '用户列表', 'uri' => 'User/index', 'icon' => 'fa-list-alt'],
                         ['name' => '添加用户', 'uri' => 'User/add', 'icon' => 'fa-plus-circle'],
                         ['name' => '修改用户', 'uri' => 'User/edit.hide', 'icon' => 'fa-pencil-square'],
                         ['name' => '回收站', 'uri' => 'User/recycle', 'icon' => 'fa-recycle'],
-                    ]
+                    ],
                 ],
             ],
         ],
-        'content' => [//内容
+        'content'  => [ //内容
             'level' => 2,
             'items' => [
                 [
-                    'name' => '栏目管理', 'group' => 'Category', 'icon' => 'fa-columns', 'lists' => 'Category',
+                    'name'  => '栏目管理', 'group' => 'Category', 'icon' => 'fa-columns', 'lists' => 'Category',
                     'items' => [
                         ['name' => '栏目列表', 'uri' => 'Category/index', 'icon' => 'fa-list-alt'],
                         ['name' => '添加栏目', 'uri' => 'Category/add', 'icon' => 'fa-plus-circle'],
                         ['name' => '编辑栏目', 'uri' => 'Category/edit.hide', 'icon' => 'fa-pencil-square'],
                         ['name' => '回收站', 'uri' => 'Category/recycle', 'icon' => 'fa-recycle'],
-                    ]
+                    ],
                 ],
                 [
-                    'name' => '文章管理', 'group' => 'Article', 'icon' => 'fa-book', 'lists' => 'Article',
+                    'name'  => '文章管理', 'group' => 'Article', 'icon' => 'fa-book', 'lists' => 'Article',
                     'items' => [
                         ['name' => '文章列表', 'uri' => 'Article/index', 'icon' => 'fa-list-alt'],
                         ['name' => '添加文章', 'uri' => 'Article/add', 'icon' => 'fa-plus-circle'],
                         ['name' => '编辑文章', 'uri' => 'Article/edit.hide', 'icon' => 'fa-pencil-square'],
                         ['name' => '回收站', 'uri' => 'Article/recycle', 'icon' => 'fa-recycle'],
-                    ]
+                    ],
                 ],
                 [
-                    'name' => '评论管理', 'group' => 'Comment', 'icon' => 'fa-comments', 'lists' => 'Comment',
+                    'name'  => '评论管理', 'group' => 'Comment', 'icon' => 'fa-comments', 'lists' => 'Comment',
                     'items' => [
                         ['name' => '评论列表', 'uri' => 'Comment/index', 'icon' => 'fa-list-alt'],
                         ['name' => '编辑评论', 'uri' => 'Comment/edit.hide', 'icon' => 'fa-pencil-square'],
                         ['name' => '回收站', 'uri' => 'Comment/recycle', 'icon' => 'fa-recycle'],
-                    ]
+                    ],
                 ],
             ],
         ],
-        'model' => [//应用
+        'model'    => [ //应用
             'level' => 2,
             'items' => [
                 [
-                    'name' => '数据库管理', 'group' => 'DB', 'icon' => 'fa-database', 'lists' => 'DB',
+                    'name'  => '数据库管理', 'group' => 'DB', 'icon' => 'fa-database', 'lists' => 'DB',
                     'items' => [
                         ['name' => '数据库备份', 'uri' => 'DB/index', 'icon' => 'fa-list-alt'],
                         ['name' => '数据库恢复', 'uri' => 'DB/undo', 'icon' => 'fa-undo'],
-                    ]
+                    ],
                 ],
                 [
-                    'name' => '插件管理', 'group' => 'Addons', 'icon' => 'fa-briefcase', 'lists' => 'Addons',
+                    'name'  => '插件管理', 'group' => 'Addons', 'icon' => 'fa-briefcase', 'lists' => 'Addons',
                     'items' => [
                         ['name' => '插件列表', 'uri' => 'Addons/index', 'icon' => 'fa-list-alt'],
                         ['name' => '回收站', 'uri' => 'Addons/recycle', 'icon' => 'fa-recycle'],
-                    ]
+                    ],
                 ],
             ],
         ],
